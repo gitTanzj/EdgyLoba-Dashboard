3b2c0baa0638601aa841dab4506ac45fd4619448
muumi <chrisrobin.soon@gmail.com>
2022-09-23 19:20:21 +0300

    feat: tailwind css changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,8 +3,8 @@ import logo from '../assets/EdgyLoba.png';
 const Header = () => {
     return (
     <div className="header">
-        <img src={logo} alt='logo'/>
-        <h1>Edgy-Loba Dashboard</h1>
+        <img src={logo} alt='logo' className="max-w-[5%]"/>
+        <h1 className="p-4 text-2xl">Edgy-Loba Dashboard</h1>
     </div>
     );
 }
